4b14de6d315dc18afccd5c6285bd8c17388ff45c
nklep001 <160364692+nklep001@users.noreply.github.com>
2026-02-16 18:04:42 -0500

    Implement recordResults function for game scoring
    Added a function to record game results and determine the winner based on scores.

--- Second.js
+++ Second.js
@@ -1,8 +1,19 @@
-const runGame = function() {
-    // Your code additions go here
+const recordResults = function() {
+    let team1 = prompt("Enter the name of the first team:");
+    let score1 = parseInt(prompt("Enter " + team1 + "'s score:"));
+    
+    let team2 = prompt("Enter the name of the second team:");
+    let score2 = parseInt(prompt("Enter " + team2 + "'s score:"));
 
+    let resultMessage = "";
 
-  
+    if (score1 > score2) {
+        resultMessage = team1 + " won " + score1 + " to " + score2;
+    } else if (score2 > score1) {
+        resultMessage = team2 + " won " + score2 + " to " + score1;
+    } else {
+        resultMessage = "It's a tie! " + score1 + " to " + score2;
+    }
   
     // Code that adds a new paragraph with a string to the "div" in the HTML file 
     const divId = document.getElementById("results");
